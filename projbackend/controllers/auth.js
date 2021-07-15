6da5411dababd993a6a327ee1bddb42dee8d0adb
DopeDevX <shreemaan.abhishek@gmail.com>
2021-07-16 00:29:28 +0545

    save user in the database on request

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,10 +1,22 @@
+const User = require("../models/user")
+
 exports.signout = (req, res) => {
     res.json({
         message: "User signout"
     })
 }
 exports.signup = (req, res) => {
-    res.json({
-        message: "User signup!!"
+    const user = new User(req.body)
+    user.save((err, user) => {
+        if(err){
+            return res.status(400).json({
+                err: "UNABLE TO SAVE USER IN DB"
+            })
+        }
+        res.json({
+            name: user.name,
+            email: user.email,
+            id: user._id
+        });
     })
 }
