a69cf5c99fa079c00ce6ab535448792ca1243719
DopeDevX <shreemaan.abhishek@gmail.com>
2021-07-17 11:02:16 +0545

    add signout and sigin functionality

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,7 +1,10 @@
 const User = require("../models/user")
 const {validationResult} = require("express-validator")
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
 
 exports.signout = (req, res) => {
+    res.clearCookie("token")
     res.json({
         message: "User signout"
     })
@@ -28,5 +31,37 @@ exports.signup = (req, res) => {
             email: user.email,
             id: user._id
         });
+    })
+}
+
+exports.signin = (req, res) => {
+    const {email, password} = req.body
+
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            error: errors.array()[0].msg,
+            param: errors.array()[0].param
+        })
+    }
+
+    User.findOne({email}, (error, user) => {
+        if(error || !user){
+            return res.status(400).json({
+                error: "USER email does not exist"
+            })
+        }
+
+        if(!user.authenticate(password)){
+            return res.status(401).json({
+                error: "Email and password do not match"
+            })
+        }
+
+        const token = jwt.sign({_id: user._id}, process.env.SECRET)
+        res.cookie("token", token, {expire: new Date() + 9999})
+
+        const {_id, name, email, role} = user
+        return res.json({token, user: {_id, name, email, role}})
     })
 }
