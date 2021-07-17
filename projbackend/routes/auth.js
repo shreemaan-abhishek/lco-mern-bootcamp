f3d219bfc129669e2e15ae7db9d441a12ad9e45a
DopeDevX <shreemaan.abhishek@gmail.com>
2021-07-17 11:00:44 +0545

    add sign in route

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,12 +1,18 @@
 var express = require("express")
 var router = express.Router()
 const {check} = require("express-validator")
-const {signout, signup} = require("../controllers/auth")
+const {signout, signup, signin} = require("../controllers/auth")
 
 router.post("/signup",
 check("name", "Name should be longer then 3 characters").isLength({ min: 3}),
 check("email", "Not an email").isEmail(),
 signup)
+
+router.post("/signin",
+check("email", "email is required").not().isEmpty(),
+check("password", "password is required").not().isEmpty(),
+signin)
+
 router.get("/signout", signout)
 
 module.exports = router
