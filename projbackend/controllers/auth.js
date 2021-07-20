11221ddf6eff538acc36f0b0c9ef9bf0c52f1714
DopeDevX <shreemaan.abhishek@gmail.com>
2021-07-20 18:40:19 +0545

    add isSignedIn and other checks

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -65,3 +65,28 @@ exports.signin = (req, res) => {
         return res.json({token, user: {_id, name, email, role}})
     })
 }
+
+exports.isSignedIn = expressJwt({
+    secret: process.env.SECRET,
+    userProperty: "auth"
+})
+
+exports.isAuthenticated = (req, res, next) => {
+    let checker = req.profile && req.auth && req.profile._id === req.auth._id
+    if(!checker){
+        return res.status(403).json({
+            error: "ACCESS DENIED"
+        })
+    }
+
+    next()
+}
+
+exports.isAdmin = (req, res, next) => {
+    if(req.profile.role === 0){
+        return res.status(403).json({
+            error: "You are not admin"
+        })
+    }
+    next()
+}
