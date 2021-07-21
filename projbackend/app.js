57762998194ba443952fd71ad8c3b2bae76dc480
DopeDevX <shreemaan.abhishek@gmail.com>
2021-07-21 17:47:57 +0545

    add user routes

--- projbackend/app.js
+++ projbackend/app.js
@@ -6,7 +6,9 @@ const app = express()
 const bodyParser = require("body-parser")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
+
 const authRoutes = require("./routes/auth")
+const userRoutes = require("./routes/user")
 
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
@@ -21,6 +23,7 @@ app.use(cookieParser())
 app.use(cors())
 
 app.use("/api", authRoutes)
+app.use("/api", userRoutes)
 
 const port = process.env.PORT || 8000;
 
