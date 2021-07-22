9af624994ea78321995169eb7f28a8fb9ebadace
DopeDevX <shreemaan.abhishek@gmail.com>
2021-07-22 22:33:50 +0545

    add route to get all the orders of a user

--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const router = express.Router()
 
-const {getUserById, getUser, updateUser} = require("../controllers/user")
+const {getUserById, getUser, updateUser, userPurchaseList} = require("../controllers/user")
 const { isSignedIn, isAuthenticated } = require("../controllers/auth")
 
 router.param("userId", getUserById)
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser)
 router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser)
+
+router.get("/order/user/:userId", isSignedIn, isAuthenticated, userPurchaseList)
 
 module.exports = router
