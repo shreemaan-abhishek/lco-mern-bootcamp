b50ce65c3599a82a4acc99fef12ebc50e6c0d603
DopeDevX <shreemaan.abhishek@gmail.com>
2021-07-22 22:34:21 +0545

    add controller logic to get all the orders for a particular user

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/user")
+const Order = require("../models/order")
 
 exports.getUserById = (req, res, next , id) => {
     User.findById(id).exec((err, user) => {
@@ -39,4 +40,17 @@ exports.updateUser = (req, res) => {
             res.json(user)
         }
     )
+}
+
+exports.userPurchaseList = (req, res) => {
+    Order.find({User: req.profile._id})
+    .populate("user", "_id name")
+    .exec((err, order) => {
+        if(err){
+            return res.status(400).json({
+                error: "No orders found"
+            })
+        }
+        return res.json(order)
+    })
 }
