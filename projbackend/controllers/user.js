3a931d5d17eebc39ef2c764053c70529bb0cbbc8
DopeDevX <shreemaan.abhishek@gmail.com>
2021-07-22 12:40:06 +0545

    add user update route and controller

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -20,3 +20,23 @@ exports.getUser = (req, res) => {
     req.profile.updatedAt = undefined
     return res.json(req.profile)
 }
+
+exports.updateUser = (req, res) => {
+    User.findByIdAndUpdate(
+        {_id: req.profile._id},
+        {$set: req.body},
+        {new: true, useFindAndModify: false},
+        (err, user) => {
+            if(err){
+                return res.status(400).json({
+                    error: "Not authorized"
+                })
+            }
+            user.salt = undefined
+            user.encry_password = undefined
+            user.createdAt = undefined
+            user.updatedAt = undefined
+            return res.json(user)
+        }
+    )
+}
