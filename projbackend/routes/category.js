5509d654ec9086883e273e2c533de868b8dfe354
shreemaan-abhishek <shreemaan.abhishek@gmail.com>
2021-07-24 17:23:24 +0545

    add routes for gettingACategory and gettingAllCategories

--- projbackend/routes/category.js
+++ projbackend/routes/category.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-const { getCategoryById, createCategory } = require("../controllers/category")
+const { getCategoryById, createCategory, getCategory, getAllCategories } = require("../controllers/category")
 const { getUserById } = require("../controllers/user")
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth")
 
@@ -15,5 +15,8 @@ router.post(
     isAdmin,
     createCategory
 )
+
+router.get("/category/:categoryId", getCategory)
+router.get("/categories", getAllCategories)
 
 module.exports = router
