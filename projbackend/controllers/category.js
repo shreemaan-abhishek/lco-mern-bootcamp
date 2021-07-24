614f47d0f1aa21812a2a3e4341243e1ebc6c030f
shreemaan-abhishek <shreemaan.abhishek@gmail.com>
2021-07-24 16:50:45 +0545

    add createCategory controller

--- projbackend/controllers/category.js
+++ projbackend/controllers/category.js
@@ -12,3 +12,16 @@ exports.getCategoryById = (req, res, next, id) => {
         next()
     })
 }
+
+exports.createCategory = (req, res) => {
+    let category = new Category(req.body)
+    category.save( (error, category) => {
+        if(error){
+            return res.status(400).json({
+                error: "Unable to save category"
+            })
+        }
+
+        res.json({ category })
+    })
+}
