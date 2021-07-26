d25a26ebdc3222aca9ebc27ba781b7991e40d2ce
shreemaan-abhishek <shreemaan.abhishek@gmail.com>
2021-07-26 15:07:07 +0545

    add routes for product

--- projbackend/app.js
+++ projbackend/app.js
@@ -10,6 +10,7 @@ const cors = require("cors")
 const authRoutes = require("./routes/auth")
 const userRoutes = require("./routes/user")
 const categoryRoutes = require("./routes/category")
+const productRoutes = require("./routes/product")
 
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
@@ -26,6 +27,7 @@ app.use(cors())
 app.use("/api", authRoutes)
 app.use("/api", userRoutes)
 app.use("/api", categoryRoutes)
+app.use("/api", productRoutes)
 
 const port = process.env.PORT || 8000;
 
