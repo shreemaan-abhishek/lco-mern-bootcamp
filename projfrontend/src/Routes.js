feb4083e93c909deabdc009740b140bd3465b732
shreemaan-abhishek <shreemaan.abhishek@gmail.com>
2021-08-04 18:06:59 +0545

    add signin and signup routes

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -1,14 +1,18 @@
 import React from "react"
 import Home from "./core/Home"
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Signup from "./user/Signup";
+import Signin from "./user/Signin";
 const Routes = () => {
-    return ( 
+    return (
         <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={Home} />
-        </Switch>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/signup" component={Signup} />
+                <Route exact path="/signin" component={Signin} />
+            </Switch>
         </BrowserRouter>
-     );
+    );
 }
- 
+
 export default Routes;
