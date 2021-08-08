e12ef33139b82b27c2af66ca6e5e53d858449d0a
shreemaan-abhishek <shreemaan.abhishek@gmail.com>
2021-08-08 16:10:23 +0545

    fix: user being able to signup without password

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -6,6 +6,8 @@ const {signout, signup, signin} = require("../controllers/auth")
 router.post("/signup",
 check("name", "Name should be longer then 3 characters").isLength({ min: 3}),
 check("email", "Not an email").isEmail(),
+check("password", "Password is required").isLength({min: 1}),
+check("password", "Password must be longer than 8 characters").isLength({min: 8}),
 signup)
 
 router.post("/signin",
