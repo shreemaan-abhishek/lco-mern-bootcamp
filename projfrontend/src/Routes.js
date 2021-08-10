d8e3972fa79fb7a26c0da828f420126b7d9ea7dd
shreemaan-abhishek <shreemaan.abhishek@gmail.com>
2021-08-10 11:17:05 +0545

    add admin and private routes

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -3,6 +3,10 @@ import Home from "./core/Home"
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Signup from "./user/Signup";
 import Signin from "./user/Signin";
+import PrivateRoute from "./auth/helper/PrivateRoutes";
+import AdminRoute from "./auth/helper/AdminRoutes";
+import UserDashBoard from "./user/UserDashBoard";
+import AdminDashBoard from "./user/AdminDashBoard"
 const Routes = () => {
     return (
         <BrowserRouter>
@@ -10,6 +14,8 @@ const Routes = () => {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/signup" component={Signup} />
                 <Route exact path="/signin" component={Signin} />
+                <PrivateRoute path="/user/dashboard" exact component={UserDashBoard} />
+                <AdminRoute path="/admin/dashboard" exact component={AdminDashBoard} />
             </Switch>
         </BrowserRouter>
     );
